183b2071ae56394695c5ab627266e64871c138ed
JRuizY2023 <148101874+JRuizY2023@users.noreply.github.com>
2023-10-27 17:04:02 -0500

    Update fechas.py

--- fechas.py
+++ fechas.py
@@ -23,3 +23,16 @@ def fechas_mes(year, month, hoja,st,row):
             hoja[s1].value=new_date
             new_date=new_date+datetime.timedelta(hours=1)
         i+=1
+
+def fecha_since_date(year, month, day, hoja, st, row):
+    new_date=datetime.datetime(year,month,day,1,0,0)
+    new_date_1=new_date-datetime.timedelta(days=30)
+    i=0
+    while new_date_1<new_date:
+        s=st
+        n=row+i
+        n1=str(n)
+        s1=s+n1
+        hoja[s1].value=new_date_1
+        new_date_1=new_date_1+datetime.timedelta(hours=1)
+        i+=1
